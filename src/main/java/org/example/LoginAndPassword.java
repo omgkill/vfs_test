a481e84d35e16cc725f59e7b171b339b319551cc
shuai.jing <shuai.jing@greatcin.com>
2024-06-19 10:30:32 +0800

    init

---
+++ src/main/java/org/example/LoginAndPassword.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class LoginAndPassword {
+
+
+    public static String LGN = "";
+
+    public static String PSWD = "";
+
+    public static String URL = "";
+
+
+    public static String USER = "";
+
+
+    public static String PASSWORD = "";
+
+    public static String identificationNumber = "";
+
+    public static String name = "";
+
+    public static String surName = "";
+
+    public static String passportNumber = "";
+    public static String countryNumber = "";
+    public static String contactNumber = "";
+    public static String dateOfBirth = "";
+    public static String endDateOfPassport = "";
+
+    public static String sex = "";
+
+
+
+
+
+
+
+
+
+    public static String emailFrom = "";
+    public static String emailPassword = "";
+    public static String emailTo = "";
+
+
+}
